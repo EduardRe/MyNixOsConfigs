7c87b31de0ce931a360494f3f9663de73c264665
Эдуард Резунов <job-asd1239911@yandex.ru>
2025-07-19 19:37:26 +0400

    feat: Добавил автологин пользователя

--- nixos-configs/configuration.nix
+++ nixos-configs/configuration.nix
@@ -48,16 +48,20 @@
   services.flatpak.enable = true;
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "ru";
-    variant = "";
+  services.xserver = {
+    enable = true;
+    displayManager.gdm = {
+      enable = true;
+      autoLogin = {
+        enable = true;
+        user = "ed";
+      };
+    };
+    desktopManager.gnome.enable = true;
+    xkb = {
+      layout = "us,ru";
+      options = "grp:alt_shift_toggle,grp_led:scroll";
+    };
   };
 
   # Enable CUPS to print documents.
