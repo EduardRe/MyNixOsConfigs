ccace91e61b21a112868122d9ef537e2570dc3ce
Эдуард Резунов <job-asd1239911@yandex.ru>
2025-07-21 00:23:25 +0400

    feat: Предварительная настройка Gnome

--- nixos-configs/home.nix
+++ nixos-configs/home.nix
@@ -1,39 +1,72 @@
-{ config, pkgs, ... }:{
-	home = {
-		username = "ed";
-		homeDirectory = "/home/ed";
-		stateVersion = "25.05";
-	};
+{ config, pkgs, ... }:
+{
+  # Настройки home-manager для пользователя ed
+  home = {
+    username = "ed";
+    homeDirectory = "/home/ed";
+    stateVersion = "25.05";
+  };
 
-	# Включаем dconf для управления настройками GNOME
- 	# programs.dconf.enable = true;
+  # Установка расширений GNOME
+  home.packages = with pkgs; [
+    gnomeExtensions.dash-to-panel
+    gnomeExtensions.just-perfection
+  ];
 
-  	# Настройка GNOME для переключения раскладок по Shift+Alt
-	dconf.settings = {
-    	"org/gnome/desktop/input-sources" = {
-      		xkb-options = ["grp:alt_shift_toggle"];
-    	};
-    	"org/gnome/desktop/wm/preferences" = {
-            button-layout = ":minimize,maximize,close";
-        };
-  	};
-	
-	programs.bash = {
-		enable = true;
-		shellAliases = {
-			rebild = "sudo nixos-rebuild switch --flake ${config.home.homeDirectory}/nixos-configs";
-			hmswitch = "home-manager switch --flake ${config.home.homeDirectory}/nixos-configs";
-			nixupdate = "nix flake update --flake ${config.home.homeDirectory}/nixos-configs";
-		};
-	};
+  # Настройка GNOME: раскладки, кнопки окон, Dash to Panel и скрытие верхней панели
+  dconf.settings = {
+    # Раскладки клавиатуры
+    "org/gnome/desktop/input-sources" = {
+      xkb-options = ["grp:alt_shift_toggle"];
+    };
+    # Кнопки в заголовках окон
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = ":minimize,maximize,close";
+    };
+    # Включение расширений
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "dash-to-panel@jderose9.github.com"
+        "hidetopbar@mathieu.bidon.ca"
+      ];
+    };
+    # Настройка Dash to Panel
+    "org/gnome/shell/extensions/dash-to-panel" = {
+      panel-position = "BOTTOM";
+      multi-monitors = true;
+      panel-sizes = "{\"0\": 32}"; # Размер панели (32 пикселя)
+      appicon-margin = 8;
+      animate-show-apps = true;
+      show-applications-button = true;
+      show-show-apps-button = true;
+      show-activities-button = false;
+      trans-use-custom-opacity = true;
+      trans-panel-opacity = 0.8;
+      # Настройка меню приложений
+      app-menu-size = 400; # Ширина меню (в пикселях, можно настроить под себя)
+      app-menu-position = "LEFT"; # Меню открывается слева
+      app-menu-show-all-apps = true; # Показывать все приложения в меню
+      app-menu-pinned-apps = []; # Очищаем закреплённые приложения (можно добавить свои)
+      app-menu-use-custom-style = true; # Включить кастомный стиль меню
+      app-menu-opacity = 0.9; # Прозрачность меню
+      window-preview-size = 150;
+      isolate-monitors = true;
+      isolate-workspaces = true;
+      hide-overview-on-startup = true;
+    };
+    # Скрытие верхней панели GNOME
+    "org/gnome/shell/extensions/just-perfection" = {
+      panel = false;
+    };
+  };
 
-	# nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-    #    "phpstorm-with-plugins"
-    #    "phpstorm"
-    # ];
-
-	# Установка плагинов для PhpStorm
-    # home.packages = with pkgs; [
-    #    (jetbrains.plugins.addPlugins jetbrains.phpstorm [ "Key Promoter X" ])
-    # ];
+  # Настройки bash с алиасами
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      rebild = "sudo nixos-rebuild switch --flake ${config.home.homeDirectory}/nixos-configs";
+      hmswitch = "home-manager switch --flake ${config.home.homeDirectory}/nixos-configs";
+      nixupdate = "nix flake update --flake ${config.home.homeDirectory}/nixos-configs";
+    };
+  };
 }
